dcad635b24eeaaf75d3466520ef72ba39569f35d
kt.nky <nakkot774@gmail.com>
2024-05-23 21:48:51 +0900

    firebase-admin key env

--- firebase.js
+++ firebase.js
@@ -3,7 +3,6 @@ const serviceAccount = require("./serviceAccountKey.json");
 
 const firebaseAdmin = admin.initializeApp({
   credential: admin.credential.cert({
-    ...serviceAccount,
     projectId: process.env.FSA_PROJECT_ID,
     privateKey: process.env.FSA_PRIVATE_KEY,
     clientEmail: process.env.FSA_CLIENT_EMAIL
