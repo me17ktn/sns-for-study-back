5b7eae41e207055e3ccb6441a6f88643dbd3020f
kt.nky <nakkot774@gmail.com>
2024-05-23 21:51:02 +0900

    firebase-admin key

--- firebase.js
+++ firebase.js
@@ -4,7 +4,7 @@ const serviceAccount = require("./serviceAccountKey.json");
 const firebaseAdmin = admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FSA_PROJECT_ID,
-    privateKey: process.env.FSA_PRIVATE_KEY,
+    privateKey: process.env.FSA_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     clientEmail: process.env.FSA_CLIENT_EMAIL
   })
 });
